3d151e644019bdfe98161e5c9d9bb890b5da77bd
ivan <ivansamusev044@gmail.com>
2023-03-01 21:42:57 +0200

    dom1

---
+++ index.js
@@ -0,0 +1,12 @@
+const elem1 = document.getElementsByClassName('elem1')
+console.log('elem1 :>> ', elem1);
+
+const elem2 = document.getElementById('elem2')
+console.log('elem1 :>> ', elem2);
+
+
+const elem3 = document.getElementsByTagName('p')
+console.log('elem3 :>> ', elem3);
+
+const elem4 = document.querySelectorAll('#elem4')
+console.log('elem4 :>> ', elem4);
